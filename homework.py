c453df6187cd48ccbb3fbadd685b3168a9cabacc
arnavmalhotra2025 <arnav.malhotra2025@gmail.com>
2025-07-17 18:02:33 +0200

    Add files via upload

---
+++ homework.py
@@ -0,0 +1,6 @@
+#summer cloths
+temperature = int(input ("what's the temprature"))
+
+if (temperature > 20("You can wear light cloths "))
+elif (temperature > 15("you need to wear sweter"))
+else ("you need o wear a jacket")
